1ae00520d44ae4982360901ac3c71a28476ce259
baoducng <baoducng@gmail.com>
2014-07-08 16:04:17 -0700

    added ngRouter for deep linking

--- js/js.js
+++ js/js.js
@@ -1,6 +1,20 @@
-var app = angular.module("myApp", ['timer', 'ui.bootstrap', 'firebase']);
-
-app.controller('TextController', function($scope, $rootScope, $interval, $firebase){
+var app = angular.module("myApp", ['timer', 'ui.bootstrap', 'firebase', 'ngRoute']);
+	
+app.config(['$routeProvider',
+	function($routeProvider) {
+	  $routeProvider
+	  	.when('/', {
+	      templateUrl: 'js/templates/setup.html'
+	    })
+	    .when('/play', {
+	      templateUrl: 'js/templates/play.html'
+	    })
+	    .otherwise({
+	      redirectTo: '/'
+	    });
+}]);
+ 
+app.controller('TextController', function($scope, $rootScope, $interval, $firebase, $location){
 	var lastLength;
 	var enemyId;
 	var storage = [];
@@ -11,9 +25,15 @@ app.controller('TextController', function($scope, $rootScope, $interval, $fireba
 	$scope.timerRunning = false;
 	$scope.gameStarted = false;
 	$scope.userName = undefined;
+	$scope.inputCode = false;
 	$scope.ready = false;
 
-	
+
+	// if ($scope.userName === undefined){
+	// 	$scope.userName = prompt('Please enter your name!');
+	// }
+
+
 //Put text in an array because ngrepeat messes up string order.
 	for (var i = 0; i < $scope.text.length; i++){
 		var obj = { 
@@ -59,7 +79,6 @@ app.controller('TextController', function($scope, $rootScope, $interval, $fireba
     $scope.$broadcast('timer-start');
     $scope.timerRunning = true;
   };
-  // do I nned stop timer?
   $scope.stopTimer = function (){
     $scope.$broadcast('timer-stop');
     $scope.timerRunning = false;
@@ -79,47 +98,50 @@ app.controller('TextController', function($scope, $rootScope, $interval, $fireba
 // ************************FIREBASE SETUP****************//	 
 	$scope.fire = $firebase(new Firebase("https://typinggame.firebaseio.com"));
 
-	//replace this with an input box later	
-	if ($scope.userName === undefined){
-		$scope.userName = prompt('Please enter your name!');
+	//inititate new user
+	$scope.setUser = function(input){
+		$scope.userName = input;
+		$scope.inputCode = true;
+		$scope.fire.$add({
+			name: $scope.userName, 
+			wpm: $scope.WPM, 
+			completion: $scope.completionRate, 
+			text: $scope.textStorage, 
+			against: null})
+		.then(function(ref){
+			$scope.userId = ref.name();	
+			$interval(function(){
+				$scope.calWPM();
+				$scope.fire.$child($scope.userId).$update({
+					wpm: $scope.WPM, 
+					completion: $scope.completionRate,
+					text: $scope.textStorage
+				});
+			}, 1000);
+			$scope.fire.$child($scope.userId).$on('value', function(snapShot){
+				if ($scope.fire.$child($scope.userId).against !== undefined){
+					enemyId = $scope.fire.$child($scope.userId).against;
+					$scope.enemy = $scope.fire.$child(enemyId);
+					$scope.ready = true;
+					$location.path('/play');
+					$scope.fire.$child($scope.userId).$off('value');
+				}
+			});					
+		});
 	}
 
-	//inititate new user
-	$scope.fire.$add({
-		name: $scope.userName, 
-		wpm: $scope.WPM, 
-		completion: $scope.completionRate, 
-		waiting: true,
-		text: $scope.textStorage, 
-		against: null})
-			.then(function(ref){
-				$scope.userId = ref.name();	
-				$interval(function(){
-					$scope.calWPM();
-					$scope.fire.$child($scope.userId).$update({
-						wpm: $scope.WPM, 
-						completion: $scope.completionRate,
-						text: $scope.textStorage
-					});
-				}, 1000);
-					$scope.fire.$child($scope.userId).$on('value', function(snapShot){
-						if ($scope.fire.$child($scope.userId).against !== undefined){
-							enemyId = $scope.fire.$child($scope.userId).against;
-							$scope.enemy = $scope.fire.$child(enemyId);
-							$scope.ready = true;
-							$scope.fire.$child($scope.userId).$off('value')
-						}
-					});
-					
-			});
-
 	$scope.addOpponents = function(code){
-		$scope.fire.$child($scope.userId).$off('value')
-		$scope.fire.$child($scope.userId).$update({against: code})
+		$scope.fire.$child($scope.userId).$off('value');
+		$scope.fire.$child($scope.userId).$update({against: code});
 		$scope.fire.$child(code).$update({against: $scope.userId});
 		$scope.enemy = $scope.fire.$child(code);
 		$scope.ready = true;
+		$location.path('/play');
 	}
 
+
 });
-	
+
+
+
+
